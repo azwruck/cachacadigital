6e11f1d53805031a1c2e7929fd58291ca0f29e9b
azwruck <andrezwruck@hotmail.com>
2018-04-27 20:05:16 -0300

    Interface Cliente

---
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/cliente/ClienteBrowse.java
@@ -0,0 +1,6 @@
+package com.wwsistemas.cachacadigital.web.cliente;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class ClienteBrowse extends AbstractLookup {
+}
---
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/cliente/ClienteEdit.java
@@ -0,0 +1,7 @@
+package com.wwsistemas.cachacadigital.web.cliente;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.wwsistemas.cachacadigital.entity.Cliente;
+
+public class ClienteEdit extends AbstractEditor<Cliente> {
+}
