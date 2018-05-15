51e5903fd1abc7e607dd619ce128cb7cb2b7833b
azwruck <andrezwruck@hotmail.com>
2018-05-14 21:54:36 -0300

    Telas

---
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/venda/Frame.java
@@ -0,0 +1,9 @@
+package com.wwsistemas.cachacadigital.web.venda;
+
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.Component;
+
+public class Frame extends AbstractWindow {
+
+}
