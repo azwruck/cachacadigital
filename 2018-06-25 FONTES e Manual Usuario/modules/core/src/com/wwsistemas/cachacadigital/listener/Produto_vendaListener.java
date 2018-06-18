02f84a838ba82bfaecf5627369437fdc9a119d2e
azwruck <andrezwruck@hotmail.com>
2018-06-18 19:12:21 -0300

    Relatórios

--- 2018-06-25 FONTES e Manual Usuario/modules/core/src/com/wwsistemas/cachacadigital/listener/Produto_vendaListener.java
+++ 2018-06-25 FONTES e Manual Usuario/modules/core/src/com/wwsistemas/cachacadigital/listener/Produto_vendaListener.java
@@ -8,32 +8,40 @@ import com.wwsistemas.cachacadigital.entity.Produto_venda;
 import com.haulmont.cuba.core.listener.BeforeUpdateEntityListener;
 
 @Component("cachaca_Produto_vendaListener")
-public class Produto_vendaListener implements BeforeInsertEntityListener<Produto_venda>, BeforeUpdateEntityListener<Produto_venda> {
+public class Produto_vendaListener
+		implements BeforeInsertEntityListener<Produto_venda>, BeforeUpdateEntityListener<Produto_venda> {
 
+	@Override
+	public void onBeforeInsert(Produto_venda entity, EntityManager entityManager) {
+		try{
+		recalculaEstoque(entity, entity.getIdProduto(), entityManager);
+		} catch(IllegalArgumentException e){
+			System.out.println(e.getMessage());
+		}
+	}
 
-    @Override
-    public void onBeforeInsert(Produto_venda entity, EntityManager entityManager) {
-    	recalculaEstoque(entity, entity.getIdProduto(), entityManager);
-    }
+	@Override
+	public void onBeforeUpdate(Produto_venda entity, EntityManager entityManager) {
+		try{
+		recalculaEstoque(entity, entity.getIdProduto(), entityManager);
+		} catch(IllegalArgumentException e){
+			System.out.println(e.getMessage());
+		}
+	}
 
+	private void recalculaEstoque(Produto_venda pv, Produto p, EntityManager em) {
 
-    @Override
-    public void onBeforeUpdate(Produto_venda entity, EntityManager entityManager) {
-    	recalculaEstoque(entity, entity.getIdProduto(), entityManager);
-    }
+			if (pv.getQuantidade() > p.getQuantidade()) {
+				throw new IllegalArgumentException("Saldo insuficiente");
+			} else {
 
-    private void recalculaEstoque(Produto_venda pv, Produto p, EntityManager em){
-        if (p == null)
-            return;
-        if(pv.getQuantidade() > p.getQuantidade()){
-        	return;
-        }
-        
-        Integer quantidade = (p.getQuantidade() - pv.getQuantidade());
-        
-        Produto pro = em.merge(p);
-        
-        pro.setQuantidade(quantidade);
-    }
+			Integer quantidade = (p.getQuantidade() - pv.getQuantidade());
+
+			Produto pro = em.merge(p);
+
+			pro.setQuantidade(quantidade);
+			}
+
+	}
 
 }
---
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/screens/Relatorio.java
@@ -0,0 +1,6 @@
+package com.wwsistemas.cachacadigital.web.screens;
+
+import com.haulmont.cuba.gui.components.AbstractWindow;
+
+public class Relatorio extends AbstractWindow {
+}
