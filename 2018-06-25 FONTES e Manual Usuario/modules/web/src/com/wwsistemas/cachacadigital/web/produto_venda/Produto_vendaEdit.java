a74c8b10cdc0f44f9e4f8e3b2cb0947d305ea877
azwruck <andrezwruck@hotmail.com>
2018-06-25 21:48:14 -0300

    Validação da venda

--- 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/produto_venda/Produto_vendaEdit.java
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/produto_venda/Produto_vendaEdit.java
@@ -1,7 +1,18 @@
 package com.wwsistemas.cachacadigital.web.produto_venda;
 
 import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.ValidationErrors;
 import com.wwsistemas.cachacadigital.entity.Produto_venda;
 
 public class Produto_vendaEdit extends AbstractEditor<Produto_venda> {
+	
+	Produto_venda pv = getItem();
+	
+	@Override
+	protected void postValidate(ValidationErrors errors) {
+	    if (pv.getQuantidade() > pv.getIdProduto().getQuantidade() ) {
+	            errors.add("City name can't contain digits");
+	        }
+	    
+	}
 }
--- 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/venda/VendaEdit.java
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/venda/VendaEdit.java
@@ -1,10 +1,19 @@
 package com.wwsistemas.cachacadigital.web.venda;
 
 import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.ValidationErrors;
 import com.wwsistemas.cachacadigital.entity.Venda;
 
 public class VendaEdit extends AbstractEditor<Venda> {
+	
+	Venda v = getItem();
+	
 
-    public void onAddClick() {
-    }
+	@Override
+	protected void postValidate(ValidationErrors errors) {
+	    if (v.getIdProdutoVenda().getQuantidade() > pv.getIdProduto().getQuantidade() ) {
+	            errors.add("City name can't contain digits");
+	        }
+	    
+	}
 }
