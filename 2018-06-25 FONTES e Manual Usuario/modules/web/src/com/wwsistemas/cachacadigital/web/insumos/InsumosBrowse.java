fbd402b669c30e54f9d9bb1cfa5c069f5bffcd64
azwruck <andrezwruck@hotmail.com>
2019-03-24 15:58:03 -0300

    Criação insumos

---
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/insumos/InsumosBrowse.java
@@ -0,0 +1,6 @@
+package com.wwsistemas.cachacadigital.web.insumos;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class InsumosBrowse extends AbstractLookup {
+}
---
+++ 2018-06-25 FONTES e Manual Usuario/modules/web/src/com/wwsistemas/cachacadigital/web/insumos/InsumosEdit.java
@@ -0,0 +1,7 @@
+package com.wwsistemas.cachacadigital.web.insumos;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.wwsistemas.cachacadigital.entity.Insumos;
+
+public class InsumosEdit extends AbstractEditor<Insumos> {
+}
